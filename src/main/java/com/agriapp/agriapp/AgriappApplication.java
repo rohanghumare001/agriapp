5e6fec7c71663aa5c265ca479cef7304745635f9
Rohan Ghumre <rohanghumare001@gmail.com>
2026-04-14 14:33:21 +0530

    first commit

---
+++ src/main/java/com/agriapp/agriapp/AgriappApplication.java
@@ -0,0 +1,13 @@
+package com.agriapp.agriapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agriapp/agriapp/AgriappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agriapp.agriapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
